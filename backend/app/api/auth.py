530c1ad866fcc05608f17f4dafdb9e9c4ecddaac
Jedrzej Ciechanowski <jk.ciechanowski@gmail.com>
2024-12-15 13:40:06 +0100

    refactor: login and register now use jwt

--- backend/app/api/auth.py
+++ backend/app/api/auth.py
@@ -1,6 +1,6 @@
-from flask import request, redirect, url_for, flash
+from flask import request, jsonify
 from app.models.user import User
-from flask_login import current_user, login_user,logout_user,login_required
+from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required
 
 
 def auth_routes(app,db):
@@ -8,35 +8,79 @@ def auth_routes(app,db):
     @app.route('/login', methods=['GET','POST'])
     def login():
         if request.method == 'POST':
-            username = 'ondre' #request.form['username'] jesli tak to dziala w react native
-            password = 'ondre' #request.form['password']
-            
+
+            if not request.is_json:
+                return jsonify(
+                    msg="Missing json in request"
+                ), 400
+
+            data = request.get_json()
+            username = data.get('username')
+            password = data.get('password')
+
+            if not username or not password:
+                return jsonify(
+                    msg="Missing username or password"
+                    ), 400
+
             user = User.query.filter_by(username=username).first()
+
+            if not user and not user.check_password(password):
+                return jsonify(
+                    msg='Login Unsuccessful. Please check username and password.'
+                ),400
             
-            if user and user.check_password(password):
-                login_user(user)
-                #flash('Login successful!', 'success')
-                return 'Login Succesful' #redirect(url_for('dashboard'))
-            else:
-                #flash('Login Unsuccessful. Please check username and password.!', 'failed')
-                return 'Login Unsuccessful. Please check username and password.'
-    
+            access_token = create_access_token(identity=username)
+            refresh_token = create_refresh_token(identity=username)
+
+            return jsonify(
+                message=f"Login Successful, {user}",
+                access_token=access_token,
+                refresh_token=refresh_token
+            ) 
+                
         return 'Login Page'
 
     @app.route('/register', methods=['GET','POST'])
     def register():
         if request.method == 'POST':
-            #data
-            #if sprawdzajace czy istnieje juz taki user 
-            #new_user = User(username='ondre', email='ondre@example.com', password='ondre')
-            #db.session.add(new_user)
-            #db.session.commit()
+            data = request.get_json()
+            email = data.get('email')
+            username = data.get('username')
+            password = data.get('password')
 
-            return 'POST register'
-        return 'Get register'
+            user_email = User.query.filter_by(email=email).first()
+            user_name = User.query.filter_by(username=username).first()
 
-    @app.route('/logout')
-    @login_required
-    def logout():
-        logout_user()
-        return 'Success logout' #redirect(url_for('login'))
+            if user_email:
+                return jsonify(
+                    msg="Email already used"
+                ), 400
+            if user_name:
+                return jsonify(
+                    msg="Username already used"
+                ), 400
+            
+            new_user = User(username=username, email=email, password=password)
+
+            db.session.add(new_user)
+            db.session.commit()
+
+            access_token = create_access_token(identity=username)
+            refresh_token = create_refresh_token(identity=username)
+
+            return jsonify(
+                    msg=f"Register Successful, {new_user}",
+                    access_token=access_token,
+                    refresh_token=refresh_token
+                ) 
+        return 'Register Page'
+
+
+    # @app.route("/refresh", methods=["POST"])
+    # @jwt_refresh_token_required
+    # def refresh():
+    #     # Get identity from the refresh token
+    #     current_user_jwt = get_jwt_identity()
+    #     new_access_token = create_access_token(identity=current_user_jwt)
+    #     return jsonify(access_token=new_access_token)
