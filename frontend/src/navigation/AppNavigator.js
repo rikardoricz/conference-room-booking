e1d6a87310573a8a51a2c7b5d8cc7942a0b71a71
rikardo <rikardoricz@gmail.com>
2024-12-29 22:17:19 +0100

    feat: add Roboto & Lato fonts

--- frontend/src/navigation/AppNavigator.js
+++ frontend/src/navigation/AppNavigator.js
@@ -2,19 +2,33 @@ import React, { useContext } from 'react';
 import { ActivityIndicator, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useFonts } from "expo-font";
 
 import { AuthContext } from '../context/AuthContext'
 import LoginScreen from '../screens/LoginScreen';
 import SignupScreen from '../screens/SignupScreen';
 import HomeScreen from '../screens/HomeScreen';
 
+import { Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from "@expo-google-fonts/roboto";
+import { Lato_300Light, Lato_400Regular, Lato_700Bold } from "@expo-google-fonts/lato";
+
+
 const Stack = createNativeStackNavigator();
 
 const AppNavigator = () => {
   const { userToken, loading } = useContext(AuthContext);
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+    Lato_300Light,
+    Lato_400Regular,
+    Lato_700Bold,
+  });
 
-  if (loading) {
-    // loading indicator while checking login status
+
+  if (loading || !fontsLoaded) {
+    // loading indicator while checking login status and loading fonts
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <ActivityIndicator size="large" />
---
+++ frontend/src/styles/GlobalStyles.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native'
+
+export const globalStyles = StyleSheet.create({
+  heading: {
+    fontSize: 24,
+    fontFamily: "Lato_400Regular",
+  },
+  body: {
+    fontSize: 16,
+    fontFamily: "Roboto_400Regular",
+  },
+  cardDesc: {
+    fontSize: 14,
+    fontFamily: "Lato_400Regular",
+  },
+  cardTitle: {
+    fontSize: 18,
+    fontFamily: "Lato_500Medium",
+  },
+  boldText: {
+    fontSize: 18,
+    fontFamily: "Roboto_700Bold",
+  },
+})
+
