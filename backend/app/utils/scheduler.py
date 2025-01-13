9aff306b1287c22b32f1709a6add4a9114c19cd8
Jedrzej Ciechanowski <jk.ciechanowski@gmail.com>
2025-01-13 20:45:34 +0100

    fix: deleted useless code

--- backend/app/utils/scheduler.py
+++ backend/app/utils/scheduler.py
@@ -6,8 +6,6 @@ def start_scheduler(app:Flask):
     scheduler = BackgroundScheduler()
     
     scheduler.add_job(lambda: run_with_app_context(app,send_notifications), 'interval', minutes=0.5)
-    scheduler.add_job(lambda: print("Hello scheduler"), 'interval', minutes=0.25)
-
     scheduler.start()
 
 def run_with_app_context(app:Flask,func):
